d95f0a689ec4e07d22079d92d1a52fc164269042
Vitor Luis Nonino <158179013+vitorlescola@users.noreply.github.com>
2026-03-14 20:07:43 -0300

    Delete my-react-app directory

--- my-react-app/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return(  //pode incluir javascript utilizando {codigo}
-        <footer>
-            <p>{new Date().getFullYear()} &copy;my-react-app</p>
-        </footer>
-    );
-}
-
-export default Footer
